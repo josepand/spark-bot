5d5c1440a38d08cc329bbd7de869128eee66e6b6
Joseph Anderson <joeanderson@hotmail.com>
2019-08-09 12:02:52 +0100

    move out

--- backend.py
+++ backend.py
@@ -97,8 +97,8 @@ class MessageHandler:
             text = re.sub(PERSON_ID, '', text).strip()
         else:
             text = message.get('text')
-            if text.startswith("<div>"):
-                text.strip("<div>").strip("</div>")
+        if text.startswith("<div>"):
+            text.strip("<div>").strip("</div>")
 
         print('message text - {}'.format(text))
         for func in cmd_list:
